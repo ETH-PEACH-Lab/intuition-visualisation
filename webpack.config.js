113f17aa3cd3f894a4b22b2c5891a81be96e2389
rainintime7 <wangsh256@outlook.com>
2024-06-02 22:27:05 +0200

    trial to deploy on github page

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/intuition-visualisation/', // Update this line
     clean: true,
   },
   mode: 'development',
